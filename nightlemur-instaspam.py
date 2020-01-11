537c59760c1da6ca4e3c9993d08af2eb08af578c
ibrahimodv <34775413+ibrahimodv@users.noreply.github.com>
2020-01-11 13:53:58 +0300

    Update nightlemur-instaspam.py

--- nightlemur-instaspam.py
+++ nightlemur-instaspam.py
@@ -128,7 +128,7 @@ def Fail(username,shit):
     + " " + shit)
 
 def Status(shit):
-    print(Fore.WHITE +" NightLemur ~" + Style.RESET_ALL
+    print(Fore.WHITE +" Night" + Force.GREEN +"Lemur ~" + Style.RESET_ALL
     + " " + shit)
 
 def DoitAnakin(reportedGuy,reportedGuyID,username,password,proxy):
